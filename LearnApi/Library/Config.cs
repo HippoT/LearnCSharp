dac440a1860850c7251289c91edf52c9c56eea2a
HippoT <minhtuan.02071995@gmail.com>
2017-08-04 16:21:13 +0700

    Create project again

---
+++ LearnApi/Library/Config.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class Config
+    {
+        public static string ConnectionString = @"server =MINHTUAN-PC\SQLEXPRESS; database=AdventureWorks2012; Trusted_connection = true;";
+    }
+}
